961ea47f68ce57209ee56dba98c2898011e3767f
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-07-22 18:44:54 +0530

    design phase of the form

--- src/components/DCandidateForm.js
+++ src/components/DCandidateForm.js
@@ -1,24 +1,79 @@
 import React, {useState, useEffect} from "react";
-import { Grid,TextField} from "@material-ui/core";
+import { Grid,TextField,withStyles} from "@material-ui/core";
+import useForm from "./useForm";
 
+const styles = theme => ({
+    root : {
+        '& .MuiTextField-root' : {
+            margin : theme.spacing(1),
+            minWidth : 230,
+        }
+    },
+})
 
-const DCandidateForm = (props) => {
+const initialFieldValues = {
+    fullName:'',
+    mobile:'',
+    age:'',
+    email:'',
+    bloodGroup:'',
+    address:''
+}
+
+const DCandidateForm = ({classes, ...props}) => {
+
+    const {
+        values,
+        setValues,
+        handleInputChange
+    } = useForm(initialFieldValues)
+
     return (
-        <form autoComplete="off" noValidate>
+        <form autoComplete="off" noValidate className={classes.root}>
             <Grid container>
                 <Grid item xs={6}>
                     <TextField
-                    name="fullName"
-                    variant="outlined"
-                    label="Full Name"
+                        name="fullName"
+                        variant="outlined"
+                        label="Full Name"
+                        value={values.fullName}
+                        onChange={handleInputChange}
                     />
+                    <TextField
+                        name="email"
+                        variant="outlined"
+                        label="Email"
+                        value={values.email}
+                        onChange={handleInputChange}
+                    />
+                    <div>Blood group</div>
                 </Grid>
                 <Grid item xs={6}>
-
+                    <TextField
+                        name="mobile"
+                        variant="outlined"
+                        label="Mobile"
+                        value={values.mobile}
+                        onChange={handleInputChange}
+                    />
+                      <TextField
+                        name="age"
+                        variant="outlined"
+                        label="Age"
+                        value={values.age}
+                        onChange={handleInputChange}
+                    />
+                      <TextField
+                        name="address"
+                        variant="outlined"
+                        label="Address"
+                        value={values.address}
+                        onChange={handleInputChange}
+                    />
                 </Grid>
             </Grid>
         </form>
     );
 }
 
-export default DCandidateForm;
+export default withStyles(styles)(DCandidateForm);
